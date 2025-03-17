d913a096ce13a8af55adec3d70a5984902bce338
https://github.com/HaizeaRL/REINFORCEMENT_LEARNING.git <haizearumayor@gmail.com>
2025-03-17 14:27:54 +0100

    clean project folders and files

--- src/get_recipes_data.py
+++ src/get_recipes_data.py
@@ -3,154 +3,141 @@ import sys
 from urllib.parse import urljoin
 import yaml
 from pathlib import Path
-import pandas as pd
-import json
+
 
 # Add the parent directory (where modules is located) to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from modules import recipe_web_scrapping_functions as rwsf
+from modules import get_data_functions as gdf
 
 # Read config file
 root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-conf = yaml.safe_load(Path(os.path.join(root_path, "config.yaml")).read_text())
+conf = yaml.safe_load(Path(os.path.join(root_path, "config.yaml")).read_text(encoding="utf-8"))
+
+# Create temporal data folder
+data_tmp_folder = os.path.join(root_path, conf["TEMPORAL_DATA_PATH"])
+os.makedirs(data_tmp_folder, exist_ok=True)
 
 # Create data folder
 data_folder = os.path.join(root_path, conf["DATA_PATH"])
 os.makedirs(data_folder, exist_ok=True)
 
-# Define save paths
-ingredients_file = os.path.join(data_folder, conf["INGREDIENTS_FILE_NAME"])
-temporal_recipes_file= os.path.join(data_folder, conf["RECIPES_FILE_NAME_TMP"])
-similar_recipes_file = os.path.join(data_folder, conf["SIMILAR_RECIPES_FILE_NAME"])
-similar_recipes_file_checked = os.path.join(data_folder, conf["SIMILAR_NEW_FILE_NAME"])
-subgroup_terms_file = os.path.join(data_folder, conf["SUBGROUP_TERMS_FILE_NAME"])
-subgroup_terms_new_file = os.path.join(data_folder, conf["SUBGROUP_NEW_FILE_NAME"])
-recipes_by_sym_and_user= os.path.join(data_folder, conf["RECIPES_COMPLETED"])
-last_recipe_file = os.path.join(data_folder, conf["RECIPES_LAST_FILE_NAME"])
-
 # Define web scrapping urls
 base_url = conf["BASE_URL"]
 url = urljoin(base_url, conf["URL_COMPLEMENT"])
 
-# If the file does not exist, execute the steps until its creation
-if not os.path.exists(similar_recipes_file):
-    # WEB SCRAP TO GET BASE RECIPE LIST   
-    print(f"STEP 1: APPLYING WEB SCRAPPING TO OBTAIN RECIPE BASE LIST: {url} ...")
-    ingredients_list, recipe_dict = rwsf.scrap_all_recipies(url, base_url)
-    # save ingredients as dataframe
-    ingredients_df = pd.DataFrame(ingredients_list, columns=['ingredients'])
-    ingredients_df.to_csv(ingredients_file,index = False)
+# STEP1: WEBSCRAP DATA save dicts to define actions to take.
+# INGREDIENTS & RECIPES
+print(f"STEP 1: APPLYING WEB SCRAPPING TO OBTAIN RECIPE BASE LIST: {url} ...")
+ingredients_list, recipe_dict = gdf.scrap_all_recipies(url, base_url)
 
-    # convert recipe dict into df
-    recipes_df = rwsf.recipe_dict_into_df(recipe_dict)
-    print(f"\tINGREDIENTS AND RECIPE BASE TABLE OBTAINED.")
+# RECIPES BY DISH_ORDER
+# get recipes by dish_order
+dish_order_categories = [conf["CATEGORY_PREFIX"]+ item for item in conf["DISH_ORDER_CATEGORIES"]]
+order_dict = gdf.scrap_recipes_by_category_list(url, base_url, dish_order_categories)
 
-    # PREPROCESS BASE RECIPE LIST: Group recipes by ingredients, get titles similarities to remove duplicates.
-    print(f"STEP 2: PREPROCESSING BASE RECIPE LIST...")
-    recipe_df_grouped = rwsf.group_titles_with_ingredients(recipes_df)
-    print(f"\tRECIPES GROUPED BY INGREDIENTS.")
+# RECIPES BY COOK TECHNIQUE
+# get techniques list
+techniques = [conf["CATEGORY_PREFIX"]+ item for item in conf["TECHNIQUES_CATEGORIES"]]
+techniques_dict = gdf.scrap_recipes_by_category_list(url, base_url, techniques)
 
-    print(f"\tGETTING SIMILAR RECIPES LIST TO BE CORRECTED BY THE USER.")    
-    # clean a little recipe titles & save recipe baseline df
-    recipe_df_grouped["cleaned"] = rwsf.process_column(conf, recipe_df_grouped["recipe"])
-    recipe_df_grouped.to_csv(temporal_recipes_file, index = False)
+# STEP2: JOIN ALL DATA INTO DF
+print(f"STEP 2: JOINING ALL DATA INTO SINGLE TABLE.")
+recipe_df = gdf.recipes_dict_to_df(recipe_dict, techniques_dict , order_dict)
+recipe_base_path= os.path.join(data_tmp_folder, "recipe_base.csv")
+recipe_df.to_csv(recipe_base_path, index = False)
 
-    # get recipe pairs & compare to find similar recipes
-    recipe_pairs = rwsf.get_recipe_pairs_to_compare(recipe_df_grouped)  # Get recipe pairs    
-    model_path = os.path.join(conf["DATA_PATH"], conf["STANZA_LANG"])
-    rwsf.compare_recipes(conf, model_path, recipe_pairs, similar_recipes_file) # Compare and save recipes
+# STEP3: CORRECT RECIPE TITLES, CORRECT RECIPE TITLES MISSPELLINGS
+print(f"STEP 3: CORRECT RECIPE TITLES MISSPELLINGS.")
 
-    # If the file exists, proceed from asking the user to mark duplicates
-    print(f"PLEASE CHECK AND MARK DUPLICATE RECIPES TO REMOVE:")
-    rwsf.get_user_input_and_update(conf, pd.read_csv(similar_recipes_file), similar_recipes_file_checked)
+# FIX RECIPE TITLES. Remove unwanted characters, non-capitalized , digits joined
+for index, row in recipe_df.iterrows():
+    recipe_df.at[index, 'recipe_new'] = gdf.correct_recipe_titles(row['recipe'])
 
-if os.path.exists(similar_recipes_file_checked):    
-    print(f"STEP 3: REMOVE DUPLICATES ACCORDING TO USER ANSWERS...")
+new_order = ["ingredient_group", "recipe","recipe_new","order", "technique", "url"]
+recipe_new_ordered = recipe_df[new_order]
+recipe_new = gdf.replace_words(recipe_new_ordered, "recipe_new", conf["DICT_TO_CHANGE"])
 
-    # get list of elements to find and remove
-    user_checked_file = pd.read_csv(similar_recipes_file_checked)
-    recipe_base_df= pd.read_csv(temporal_recipes_file)
+#save new data
+recipe_base_corrected_path= os.path.join(data_tmp_folder, "recipe_titles_corrected.csv")
+recipe_new.to_csv(recipe_base_corrected_path, index = False)
 
-    # discover rows to remove according to user, discover coindicencies and remove wrong items
-    wrong_recipe_titles = user_checked_file[user_checked_file["equal"] == 1]["incorrect"].tolist()
-    recipe_base_df = recipe_base_df[~recipe_base_df["cleaned"].isin(wrong_recipe_titles)]
-    print(f"\t{len(wrong_recipe_titles)} RECIPES REMOVE FROM TABLE.")
-    
-    # save modified recipe table again
-    recipe_base_df.to_csv(temporal_recipes_file, index = False)
+# STEP4: GROUP RECIPES BY INGREDIENTS
+print(f"STEP 4: GROUP RECIPES BY INGREDIENTS.")
+recipes_group = gdf.group_titles_with_ingredients(recipe_new)
 
-if not os.path.exists(os.path.join(data_folder,"order_dict.txt")) and \
-    not os.path.exists(os.path.join(data_folder,"techniques_dict.txt")):  
+# STEP5: REMOVE DUPLICATED RECIPES
+print(f"STEP 5: REMOVING DUPLICATED RECIPES.")
+duplicated_recipes = ["legatza_kokotea","esne_frijitua","txokolate_trufa_bonboia"]
+no_duplicated_recipes = recipes_group[~recipes_group["recipe_new"].isin(duplicated_recipes)]
 
-    print(f"STEP 4: APPLYING WEB SCRAPPING TO OBTAIN RECIPE COOK TECHNIQUES & DISH_ORDER RELATION FROM: {url} ...")
-    
-    # get techniques
-    techniques = [conf["CATEGORY_PREFIX"]+ item for item in conf["TECHNIQUES_CATEGORIES"]]
-    techniques_dict = rwsf.scrap_recipes_by_category_list(url, base_url, techniques)
-    print(f"\tRECIPES BY COOK TECHNIQUES OBTAINED. GROUPED IN TOTAL OF {len(techniques_dict.keys())} COOK TECHNIQUES.")
+#save new data
+recipe_base_grouped_path= os.path.join(data_tmp_folder, "recipe_titles_grouped.csv")
+no_duplicated_recipes.to_csv(recipe_base_grouped_path, index = False)
 
-    # get dish order recipes relation
-    dish_order_categories = [conf["CATEGORY_PREFIX"]+ item for item in conf["DISH_ORDER_CATEGORIES"]]
-    order_dict = rwsf.scrap_recipes_by_category_list(url, base_url, dish_order_categories)
-    print(f"\tRECIPES BY DISH ORDER OBTAINED. GROUPED IN: {order_dict.keys()}.")
+# STEP6: FIND MISS DISH ORDER RECIPES 
+print(f"STEP 6: FIND MISS DISH ORDER RECIPES .")
+no_order, without_order = gdf.count_empty_dish_order(no_duplicated_recipes)
+print(f"\tExist {without_order} recipes without dish_order information. Try to complete by ingredients logic.")
 
-    # save tech, dish_order dicts and last recipe table to validate results
-    with open(os.path.join(data_folder,"order_dict.txt"), "w") as f:
-        json.dump(order_dict, f, indent=4)
+# AND TRY TO COMPLETE THEM by ingredients logic, i.e.: haragia, arraina second dishes.. 
+dish_order_completed = gdf.complete_dish_order(no_duplicated_recipes, conf)
+no_order, without_order = gdf.count_empty_dish_order(dish_order_completed)
+print(f"\tAfter correct exist {without_order} recipes without dish_order information. Try to complete asking user.")
 
-    with open(os.path.join(data_folder,"techniques_dict.txt"), "w") as f:
-        json.dump(techniques_dict, f, indent=4)
+# ASK USER TO COMPLETE THOSE RECIPES WITHOUT DISH ORDER
+dish_order_completed2 = gdf.complete_dish_order_by_user(dish_order_completed, no_order, conf)   
 
-if os.path.exists(os.path.join(data_folder,"order_dict.txt")) and \
-    os.path.exists(os.path.join(data_folder,"techniques_dict.txt")):  
+#save to check
+dish_order_completed_path= os.path.join(data_tmp_folder, "recipe_with_dish_order_completed.csv")
+dish_order_completed2.to_csv(dish_order_completed_path, index = False)
 
-    # Recover dictionaries
-    with open(os.path.join(data_folder, "order_dict.txt"), "r") as f:
-        order_dict = json.load(f)
+# STEP7: FIND MISS COOK TECHNIQUE ITEMS
+print(f"STEP 7: FIND MISS COOK TECHNIQUE RECIPES .")
+no_tech, without_tech = gdf.count_empty_techniques(dish_order_completed2)
+print(f"\tExist {without_tech} recipes without cook techniques information. Try to complete by term logic.")
 
-    with open(os.path.join(data_folder, "techniques_dict.txt"), "r") as f:
-        techniques_dict = json.load(f)
+#  AND TRY TO FIX THEM by technique synonims logic, i.e.: uretan <-> egosi.. 
+recipes_with_techs = gdf.complete_tech_by_synonims(dish_order_completed2, conf)
+no_tech, without_tech = gdf.count_empty_techniques(recipes_with_techs)
+print(f"\tAfter correct exist {without_tech} recipes without cook techniques information.")
 
-    # and base df    
-    recipe_base_df= pd.read_csv(temporal_recipes_file)
+#save to check
+technique_completed_path= os.path.join(data_tmp_folder, "recipe_with_techs_completed.csv")
+recipes_with_techs.to_csv(technique_completed_path, index = False)
 
-    print(f"STEP 5: JOINING RECIPE TABLE WITH COOK TECHNIQUE AND DISH ORDER")
-    recipe_base_df["order"] = None
-    recipe_base_df["technique"] = None
-    for index, row in recipe_base_df.iterrows(): 
-        recipe_base_df.at[index, "order"] = rwsf.find_category_by_recipe(order_dict, row["recipe"])
-        recipe_base_df.at[index, "technique"] = rwsf.find_category_by_recipe(techniques_dict, row["recipe"])
 
-    recipe_base_df.to_csv(temporal_recipes_file, index = False)
-   
-    print("STEP 6: COMPLETE MISSING VALUES")
-    print("CHECKING DISH ORDER AND COMPLETING...")
-    recipe_df_new = rwsf.complete_dish_order_by_synomims_and_user(recipe_base_df, conf)
+# STEP8: TRY TO CLASIFY NO TECH RECIPES BY ORIGIN i.e.: tagliatelle, caprese <-> italia.. 
+print(f"STEP 8: TRY TO CLASIFY NO TECH RECIPES BY ITS ORIGIN .")
+recipe_df_new = gdf.complete_origin(recipes_with_techs, conf)
+no_tech_orig, without_tech_orig = gdf.count_empty_tech_and_origin(recipe_df_new)
+print(f"\tThere are {without_tech_orig} recipes without cook technique and origin.")  
 
-    '''path = os.path.join(data_folder, "recipe_tmp1.csv")
-    recipe_df_new.to_csv(path, index = False)'''
+#save to check
+origin_completed_path= os.path.join(data_tmp_folder, "recipe_with_origin_completed.csv")
+recipe_df_new.to_csv(origin_completed_path, index = False)
 
-    print("CHECKING COOK TECHNIQUES AND COMPLETING BY SYNONYMS...")
-    no_tech, without_tech = rwsf.count_empty_techniques(recipe_df_new)
-    if without_tech !=0:
-        print(f"\tThere are {without_tech} recipes without cook technique. Let's try to complete with synonims...")
-        recipe_df_new = rwsf.complete_tech_by_synonims(recipe_df_new, conf)
-        no_tech1, without_tech1 = rwsf.count_empty_techniques(recipe_df_new)
-        if without_tech1 !=0: 
-            print(f"\tAfter corrected with synonims are {without_tech1} recipes without cook technique.")
-            
-    '''path = os.path.join(data_folder, "recipe_tmp2.csv")
-    recipe_df_new.to_csv(path, index = False)'''
+# STEP9: REPLACE RECIPES TEXT TO DETECT EASIER: MORE INGREDIENTS
+print(f"STEP 9: JOIN COMPOSED TERMS IN RECIPES TO DETECT EASIER: MORE INGREDIENTS.")
+recipe_df_corrected = gdf.replace_recipe_words(recipe_df_new, "recipe_new", conf["DICT_TO_JOIN"])
 
-    print("CHECKING IF RECIPES CAN BE CLASSIFIED BY ORIGINS...")
-    recipe_df_new = rwsf.complete_origin(recipe_df_new, conf)
-    no_tech_orig, without_tech_orig = rwsf.count_empty_tech_and_origin(recipe_df_new)
-    if without_tech_orig !=0:
-        print(f"\tThere are {without_tech_orig} recipes without cook technique and origin.")   
+# STEP10: GET MORE INGREDIENTS PER RECIPES
+print(f"STEP 10: GET INGREDIENTS BY RECIPES.")
+unwanted_words = gdf.obtain_nowanted_terms(conf) + conf["UNWANTED_OTHER_TERMS"]
+recipes_with_ingredients = gdf.get_recipe_ingredients(recipe_df_corrected, "recipe_new", "ingredients", unwanted_words)
 
-    # save changed df
-    recipe_df_new.to_csv(recipes_by_sym_and_user, index = False)
+# STEP11: CORRECT INGREDIENTS MISSPELLINGS 
+print(f"STEP 11: CORRECT RECIPE TITLES AND INGREDIENTS MISSPELLINGS.")
+# correct recipe titles and column name
+recipes_with_ingredients["recipe_new"] = recipes_with_ingredients["recipe_new"].apply(lambda x: gdf.correct_capitalization(x, "recover"))
+recipes_with_ingredients.rename(columns={'recipe_new': 'recipe'}, inplace=True)
 
-if os.path.exists(recipes_by_sym_and_user):  
-    print("STEP 7: GET INGREDIENT SUBGROUPS AND CORRECT MISSPELLINGS..")
+# remove ingredients suffixes 
+recipe_ingredients_corrected = gdf.remove_suffixes(recipes_with_ingredients, "ingredients", conf["SUFFIXES"])
+
+#save to check
+recipes_extra_ingredients_path= os.path.join(data_tmp_folder, "recipes_with_ingredients.csv")
+recipe_ingredients_corrected.to_csv(recipes_extra_ingredients_path, index = False)
+print(f"\tCheck {recipes_extra_ingredients_path} file and correct minor ingredients terms.")
+print(f"\tSave final file here: {conf['DATA_PATH']} as {conf['RECIPES_FILE_NAME']}")
+print(f"END")
