a9b3aaa3e8f540e7e3dd11aab849194abe52940e
https://github.com/HaizeaRL/REINFORCEMENT_LEARNING.git <haizearumayor@gmail.com>
2025-03-18 12:40:47 +0100

    database creation script added.

---
+++ modules/database_functions.py
@@ -0,0 +1,342 @@
+import sqlite3
+from openpyxl import load_workbook
+import pandas as pd
+
+def create_db(db_path):
+  
+    # Create the database
+    conn = sqlite3.connect(db_path)
+
+    # get cursor
+    cursor = conn.cursor()
+
+    # Create category table
+    cursor.execute('''
+       CREATE TABLE ingredient_category (
+            category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ingredient_category TEXT NOT NULL UNIQUE
+        );
+        ''')
+    
+    
+
+    # Create ingredients table: ingredient related to ingredient_category
+    cursor.execute('''
+       CREATE TABLE ingredient (
+            ingredient_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ingredient TEXT NOT NULL UNIQUE,
+            category_id INTEGER,
+            FOREIGN KEY (category_id) REFERENCES ingredient_category(category_id) ON DELETE SET NULL
+        );
+        ''')
+
+    # Create dish_order table
+    cursor.execute('''
+        CREATE TABLE dish_order (
+            dish_order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            dish_order TEXT NOT NULL UNIQUE
+        );
+    ''')
+
+    # Create cook techniques table
+    cursor.execute('''
+        CREATE TABLE cook_technique (
+            cook_technique_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            cook_technique TEXT NOT NULL UNIQUE
+        );
+    ''')
+
+    # Create recipe origin table
+    cursor.execute('''
+        CREATE TABLE originate (
+            originate_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            originate TEXT NOT NULL UNIQUE
+        );
+    ''')
+
+    # Create recipe - dish-order, cook-technique , originate table, & url
+    cursor.execute('''
+        CREATE TABLE recipe (
+            recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            recipe TEXT NOT NULL UNIQUE,
+            dish_order_id INTEGER,
+            cook_technique_id INTEGER,
+            originate_id INTEGER,
+            url TEXT NOT NULL,
+            FOREIGN KEY (dish_order_id) REFERENCES dish_order(dish_order_id) ON DELETE SET NULL,
+            FOREIGN KEY (cook_technique_id) REFERENCES cook_technique(cook_technique_id) ON DELETE SET NULL,
+            FOREIGN KEY (originate_id) REFERENCES originate(originate_id) ON DELETE SET NULL
+        );
+    ''')
+
+    # recipe_ingredient_category relation table
+    cursor.execute('''
+        CREATE TABLE recipe_ingredient_category (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Unique ID for each entry
+            recipe_id INTEGER  NOT NULL,
+            category_id INTEGER  NOT NULL, 
+            FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE,
+            FOREIGN KEY (category_id) REFERENCES ingredient_category(category_id) ON DELETE CASCADE
+        );
+    ''')
+
+    # recipe_ingredient relation table
+    cursor.execute('''
+        CREATE TABLE recipe_ingredient (
+            recipe_ingredient_id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Unique ID for each entry
+            recipe_id INTEGER NOT NULL,
+            ingredient_id INTEGER,  -- Nullable field
+            FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id) ON DELETE CASCADE,
+            FOREIGN KEY (ingredient_id) REFERENCES ingredient(ingredient_id) ON DELETE CASCADE
+        );
+    ''')
+
+    # commit and close
+    conn.commit()
+    conn.close()
+
+def check_database_tables(db_path):
+    # Connect to the database
+    conn = sqlite3.connect(db_path)
+
+    # get cursor
+    cursor = conn.cursor()
+
+    # Query to check if the tables exist
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    
+    # Print out the list of tables
+    print("Tables in the database:")
+    for table in tables:
+        if table[0] != "sqlite_sequence":
+            print("\t- ",table[0])  # Print the name of each table
+    
+    # Close the connection
+    conn.close()
+
+def get_ingredient_categories(df):
+    ingredient_categories=[]
+    for index, row in df.iterrows():
+        groups = row["ingredient_group"].split(",")
+        for group in groups:
+            if group.strip() not in ingredient_categories:
+                ingredient_categories.append(group.strip())
+    return sorted(ingredient_categories)
+
+def save_list_in_db(db_path, table_column, value_list):
+
+    # open database connection
+    conn = sqlite3.connect(db_path)
+
+    # get cursor
+    cursor = conn.cursor()
+
+    # Dynamically construct the SQL query with table and column names
+    query = f"INSERT OR IGNORE INTO {table_column} ({table_column}) VALUES (?)"
+
+    # Insert values
+    cursor.executemany(query, [(value,) for value in value_list])
+
+    # commit and close
+    conn.commit()
+    conn.close()
+
+
+def save_ingredient_categories(db_path, excel_file_path, category_list):
+
+    # open database connection
+    conn = sqlite3.connect(db_path)
+
+    # get cursor
+    cursor = conn.cursor()
+
+    # open excel file to check sheetnames
+    wb = load_workbook(excel_file_path, read_only=True) 
+
+    # get each categories ingredients to insert into table
+    for category in category_list:
+        if category in wb.sheetnames:
+            
+            # get category_id
+            select_query = "SELECT category_id FROM ingredient_category WHERE ingredient_category = ?"
+            cursor.execute(select_query, (category,))
+            res = cursor.fetchone()
+            category_id = res[0] if res else None
+
+            if not category_id:
+                continue  # jump if category not exist
+
+            # read ingredients from corresponding sheet
+            df1 = pd.read_excel(excel_file_path, sheet_name=category)
+            ingredient_list = sorted(df1["ingredients"].dropna().unique().tolist())
+
+            if ingredient_list:
+                # Insert data in bbdd
+                insert_query = "INSERT OR IGNORE INTO ingredient (ingredient, category_id) VALUES (?, ?)"
+                cursor.executemany(insert_query, [(value, category_id) for value in ingredient_list])
+
+    # commit and close
+    conn.commit()
+    conn.close()
+
+def save_recipe(db_path, df):
+
+    # Connect to the database
+    conn = sqlite3.connect(db_path)
+
+    # get cursor
+    cursor = conn.cursor()
+
+    # iterate recipe df to complete ddbb recipe table
+    for index, row in df.iterrows():
+        # get recipe title and url
+        recipe = row["recipe"]   
+        url = row["url"]
+
+        # get dish_order_id
+        dish_order_id = None
+        if row["order"] is not None:
+            select_query = "SELECT dish_order_id FROM dish_order WHERE dish_order = ?"
+            cursor.execute(select_query, (row["order"],))
+            res = cursor.fetchone()
+            dish_order_id = res[0] if res else None
+            
+        # get cook_technique_id
+        cook_technique_id = None
+        if row["technique"] is not None:
+            select_query = "SELECT cook_technique_id FROM cook_technique WHERE cook_technique = ?"
+            cursor.execute(select_query, (row["technique"],))
+            res = cursor.fetchone()
+            cook_technique_id = res[0] if res else None
+            
+        # get originate_id
+        originate_id = None
+        if row["origin"] is not None:
+            select_query = "SELECT originate_id FROM originate WHERE originate = ?"
+            cursor.execute(select_query, (row["origin"],))
+            res = cursor.fetchone()
+            originate_id = res[0] if res else None
+
+        # create insert query
+        insert_query = "INSERT OR IGNORE INTO recipe (recipe, dish_order_id, cook_technique_id, originate_id, url) VALUES (?, ?, ?, ?, ?)"
+        cursor.execute(insert_query, (recipe, dish_order_id, cook_technique_id, originate_id, url))
+
+    # commit and close
+    conn.commit()
+    conn.close()
+
+def save_recipe_ingredient_category(db_path, df):
+
+    # Connect to the database
+    conn = sqlite3.connect(db_path)
+
+    # get cursor
+    cursor = conn.cursor()
+
+    # iterate recipe df to complete ddbb recipe_ingredient_category table
+    for index, row in df.iterrows():
+        # get recipe and recipe group values
+        recipe = row["recipe"]
+        ingredient_group = row["ingredient_group"]
+        
+        # get corresponding information
+        if ingredient_group is not None and recipe is not None:
+            # split categories
+            groups = ingredient_group.split(", ")
+            print(groups)
+
+            # for each group get info and insert values
+            for group in groups:
+
+                # get recipe_id
+                select_query = "SELECT recipe_id FROM recipe WHERE recipe = ?"
+                cursor.execute(select_query, (recipe,))
+                res = cursor.fetchone()
+                recipe_id = res[0] if res else None
+
+                # get category_id
+                select_query = "SELECT category_id FROM ingredient_category WHERE ingredient_category = ?"
+                cursor.execute(select_query, (group,))
+                res = cursor.fetchone()
+                ingredient_category_id = res[0] if res else None
+
+                # if data is obtained insert into database table
+                if not recipe_id :
+                    print(f"ERROR recipe: {recipe} does not exist.")
+                    continue  # jump if category not exist
+                else:
+                    # create insert query
+                    insert_query = "INSERT OR IGNORE INTO recipe_ingredient_category (recipe_id, category_id ) VALUES (?, ?)"
+                    cursor.execute(insert_query, (recipe_id, ingredient_category_id))
+
+    # commit and close
+    conn.commit()
+    conn.close()
+
+def save_recipe_ingredient(db_path, df):
+
+    # Connect to the database
+    conn = sqlite3.connect(db_path)
+
+    # get cursor
+    cursor = conn.cursor()
+
+    # iterate recipe df to complete ddbb recipe_ingredient table
+    for index, row in df.iterrows():
+        recipe = row["recipe"]
+
+        if not pd.isna(row["ingredients"]):   
+            ingredients = row["ingredients"].split(", ")
+            for ingredient in ingredients:
+
+                # get recipe_id
+                select_query = "SELECT recipe_id FROM recipe WHERE recipe = ?"
+                cursor.execute(select_query, (recipe,))
+                res = cursor.fetchone()
+                recipe_id = res[0] if res else None
+
+                # get ingredient_id if exist
+                ingredient_id = None
+                if ingredient is not None:
+                    select_query = "SELECT ingredient_id FROM ingredient WHERE ingredient = ?"
+                    cursor.execute(select_query, (ingredient,))
+                    res = cursor.fetchone()
+                    ingredient_id = res[0] if res else None
+
+                # create dynamic query and insert data
+                if recipe_id:
+                    # create insert query
+                    insert_query = "INSERT OR IGNORE INTO recipe_ingredient (recipe_id, ingredient_id) VALUES (?, ?)"
+                    cursor.execute(insert_query, (recipe_id, ingredient_id))
+
+    # commit and close
+    conn.commit()
+    conn.close()
+
+def show_table_data(db_path, table, limit=None):
+    # Connect to the database
+    conn = sqlite3.connect(db_path)
+
+    # get cursor
+    cursor = conn.cursor()
+
+    # Dynamically construct the SQL query with table and column names
+    if limit:
+        query = f"SELECT * FROM {table} LIMIT 10;"
+    else:
+        query = f"SELECT * FROM {table};"
+    
+    # Query to check data in the specified table (if any)
+    cursor.execute(query)  # Retrieve all rows
+    ingredients = cursor.fetchall()
+    
+    if len(ingredients) >0:
+        print(f"\nSample data from '{table}' table:")
+        for row in ingredients:
+            print(row)
+    else:
+        print(f"'{table}' table is empty.")
+
+    # Close the connection
+    conn.close()
---
+++ src/create_database.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import yaml
+from pathlib import Path
+import pandas as pd
+
+# Add the parent directory (where modules is located) to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from modules import database_functions as dbf
+
+
+# Read config file
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+conf = yaml.safe_load(Path(os.path.join(root_path, "config.yaml")).read_text(encoding="utf-8"))
+
+# Reference to data folder
+data_folder = os.path.join(root_path, conf["DATA_PATH"])
+os.makedirs(data_folder, exist_ok=True)
+
+# Create database and tables
+db_path = os.path.join(data_folder, conf["DATABASE_NAME"])
+dbf.create_db(db_path)
+
+# Check database creation
+dbf.check_database_tables(db_path)
+
+# Obtain data and insert to tables
+# ingredient_categories
+recipe_df = pd.read_csv(os.path.join(data_folder, conf["RECIPES_FILE_NAME"]), sep=";")
+ingredient_categories = dbf.get_ingredient_categories(recipe_df)
+dbf.save_list_in_db(db_path, "ingredient_category", ingredient_categories)
+
+# dish_order
+dish_order_l = sorted(recipe_df["order"].unique().tolist())
+dbf.save_list_in_db(db_path, "dish_order", dish_order_l)
+
+# cook_technique
+techniques_l = sorted([item for item in recipe_df["technique"].unique().tolist() if pd.notna(item)])
+dbf.save_list_in_db(db_path, "cook_technique", techniques_l)
+
+# originate
+originate_l = sorted([item for item in recipe_df["origin"].unique().tolist() if pd.notna(item)])
+dbf.save_list_in_db(db_path, "originate", originate_l)
+
+# ingredient 
+excel_file_path = os.path.join(data_folder,conf["INGREDIENT_CATEGORIES_FILENAME"])
+dbf.save_ingredient_categories(db_path, excel_file_path, ingredient_categories)
+
+# recipes
+dbf.save_recipe(db_path, recipe_df)
+
+# recipe_ingredient_category
+dbf.save_recipe_ingredient_category(db_path, recipe_df)
+
+# recipe_ingredient
+dbf.save_recipe_ingredient(db_path, recipe_df)
+
+
+# check data
+dbf.show_table_data(db_path, "ingredient_category")
+dbf.show_table_data(db_path, "dish_order")
+dbf.show_table_data(db_path, "cook_technique")
+dbf.show_table_data(db_path, "originate")
+dbf.show_table_data(db_path, "ingredient", True) # limit result to 10
+dbf.show_table_data(db_path, "recipe", True) # limit result to 10 
+dbf.show_table_data(db_path, "recipe_ingredient_category", True) # limit result to 10 
+dbf.show_table_data(db_path, "recipe_ingredient", True) # limit result to 10
